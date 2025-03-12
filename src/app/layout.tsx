dc6a658d0301825ba7bde7454ce26ae888827832
Geovani <geovanigcs.dev@gmail.com>
2025-03-12 12:30:20 -0300

    Style: Update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Calistoga, Inter } from 'next/font/google'
 import "./globals.css";
 import { twMerge } from "tailwind-merge";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ['latin'], variable: '--font-sans' })
 const calistoga = Calistoga({ subsets: ['latin'], variable: '--font-serif', weight: ["400"] })
@@ -18,9 +19,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head>
+      <Head>
         <link rel="icon" href="/favicon.png" type="image/png" />
-      </head>
+      </Head>
       <body className={twMerge(inter.variable, calistoga.variable, "bg-[#030712] text-white antialiased font-sans")}>{children}</body>
     </html>
   );
